4bdf492727809b69ca9259dc229bd6d545b50985
AdryDev <adrydevmateo@gmail.com>
2025-02-08 00:04:14 -0400

    changes

--- src/routes/home.route.tsx
+++ src/routes/home.route.tsx
@@ -7,7 +7,7 @@ const homeRoute = createRoute({
   component: function Home() {
     return (
       <div className="p-2">
-        <h3>Welcome Home!</h3>
+        <h1>Welcome Home!</h1>
       </div>
     )
   }
